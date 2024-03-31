033262ee57abd5a5b7f15146c6a4641b807ceafe
Your Name <you@example.com>
2024-03-31 11:19:14 +0800

    change

---
+++ app/src/main/java/com/example/timemanage/AddDialog.java
@@ -0,0 +1,37 @@
+package com.example.timemanage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+
+public class AddDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_dialog);
+//        Spinner week_spinner = (Spinner) findViewById(R.id.week_spinner);
+//        Spinner time_spinner_1 = (Spinner) findViewById(R.id.time_spinner_1);
+//        Spinner time_spinner_2 = (Spinner) findViewById(R.id.time_spinner_2);
+//        ArrayList<String> arrayList = new ArrayList<>();
+//        ArrayList<String> arrayList2 = new ArrayList<>();
+//        arrayList.add("周一");
+//        arrayList.add("周二");
+//        arrayList.add("周三");
+//        arrayList.add("周四");
+//        arrayList.add("周五");
+//        arrayList.add("周六");
+//        arrayList.add("周日");
+//        arrayList2.add("1");arrayList2.add("2");arrayList2.add("3");arrayList2.add("4");arrayList2.add("5");
+//        arrayList2.add("6");arrayList2.add("7");arrayList2.add("8");arrayList2.add("9");arrayList2.add("10");
+//        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,arrayList);
+//        ArrayAdapter<String> arrayAdapter2 = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,arrayList2);
+//        week_spinner.setAdapter(arrayAdapter);
+//        time_spinner_1.setAdapter(arrayAdapter2);
+//        time_spinner_2.setAdapter(arrayAdapter2);
+    }
+}
--- app/src/main/java/com/example/timemanage/MainActivity.java
+++ app/src/main/java/com/example/timemanage/MainActivity.java
@@ -1,20 +1,68 @@
 package com.example.timemanage;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 import androidx.appcompat.widget.Toolbar;
 
-import java.security.SecurityPermission;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    public void createAddDialog() {
+        //创建 一个提示对话框的构造者对象
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("添加课程");//设置弹出对话框的标题
+        //builder.setIcon(R.drawable.ic_launcher);//设置弹出对话框的图标
+        builder.setCancelable(false);//能否被取消
+
+
+        View view = LayoutInflater.from(this).inflate(R.layout.activity_add_dialog, null);
+        Spinner week_spinner = (Spinner) view.findViewById(R.id.week_spinner);
+        Spinner time_spinner_1 = (Spinner) view.findViewById(R.id.time_spinner_1);
+        Spinner time_spinner_2 = (Spinner) view.findViewById(R.id.time_spinner_2);
+        ArrayList<String> arrayList = new ArrayList<>();
+        ArrayList<String> arrayList2 = new ArrayList<>();
+        arrayList.add("周一");
+        arrayList.add("周二");
+        arrayList.add("周三");
+        arrayList.add("周四");
+        arrayList.add("周五");
+        arrayList.add("周六");
+        arrayList.add("周日");
+        arrayList2.add("1");arrayList2.add("2");arrayList2.add("3");arrayList2.add("4");arrayList2.add("5");
+        arrayList2.add("6");arrayList2.add("7");arrayList2.add("8");arrayList2.add("9");arrayList2.add("10");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,arrayList);
+        ArrayAdapter<String> arrayAdapter2 = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,arrayList2);
+        week_spinner.setAdapter(arrayAdapter);
+        time_spinner_1.setAdapter(arrayAdapter2);
+        time_spinner_2.setAdapter(arrayAdapter2);
+
+        builder.setView(view);
+        builder.setPositiveButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "那你还要不要再吃点吧", Toast.LENGTH_SHORT).show();
+                dialog.cancel();
+            }
+        });
+        builder.setNegativeButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "那你就吃一点吧", Toast.LENGTH_SHORT).show();
+                dialog.cancel();
+            }
+        });
+        builder.show();
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.toolbar_menu,menu);
@@ -28,13 +76,7 @@ public class MainActivity extends AppCompatActivity {
             //mDrawerLayout.openDrawer(GravityCompat.START);
             //break;
             case R.id.add:
-
-//            case R.id.backup:
-//                Toast.makeText(this, "KeithXiaoY clicked  Backup", Toast.LENGTH_SHORT).show();
-//                break;
-//            case R.id.delete:
-//                Toast.makeText(this, "KeithXiaoY clicked Delete", Toast.LENGTH_SHORT).show();
-//                break;
+                createAddDialog();
             default:
         }
         return true;
